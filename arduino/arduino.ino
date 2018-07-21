149182f68ba445648b308ce9f8d1cda4ac798008
groxni <nishukai@hotmail.com>
2018-07-22 00:24:51 +0800

    Backend init

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,17 +1,22 @@
-int tic = 0;
-
 void setup() {
+    Serial.begin(9600);
     pinMode(13, OUTPUT);
 }
 
 void loop() {
     //int state = analogRead(A0); 
-    int state= 0;
-    if(tic<10){
-    digitalWrite(13, HIGH);
-    delay(1000);
-    }else{
-    digitalWrite(13, LOW);
-    delay(1000);}
-    tic += 1;
-}
+    //int state= 0;
+    //if(tic<10){
+    //digitalWrite(13, HIGH);
+    //delay(1000);
+    //}else{
+    //digitalWrite(13, LOW);
+    //delay(1000);}
+    // tic += 1;
+//    if(Serial.available() > 0)
+//    {
+//      String Read = Serial.readString();
+//      Serial.print(Read);
+//    }
+      Serial.print("Some data\n");
+    }
